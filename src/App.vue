79274ed03682ea9b61114ea532aa09cbc0de8897
Hassan Issa Hassan <hassainjr@gmail.com>
2022-11-25 00:04:41 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+
+import MyFooter from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Footer: MyFooter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+    <div class="col-md-4">
+      <img src="../assets/i-can-farm.jpg">
+
+    </div>
+
+    <div class="col-md-2">
+      <h4>About</h4>
+      <ul>
+        <li><a href="">Story</a><li>
+        <li><a href="">Projects</a></li>
+      </ul>  
+
+    </div>  
+
+    <div class="col-md-2">
+      <h4>Social</h4>
+      <ul>
+        <li><a href="">Facebook</a></li>
+        <li><a href="">Instagram</a></li>
+        <li><a href="">Twitter</a></li>
+      </ul> 
+    </div>  
+    </div>
+  </footer>
+</template>
+
+
